178842c93ed5ba883a728a6d7228dcddeaae1cec
John <olylanboy@hotmail.com>
2020-10-07 17:55:40 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,4 +13,4 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Lienol/openwrt-package
 sed -i '$a src-git lienol https://github.com/xiaorouji/openwrt-package.git' feeds.conf.default
-sed -i '$a src-git myoko https://github.com/Myoko/openwrt-packages.git' feeds.conf.default
+sed -i '$a src-git diy https://github.com/Myoko/openwrt-packages.git' feeds.conf.default
