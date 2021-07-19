ee6f12394842d452ec0c1a8c753da7e955c6c7a9
John <olylanboy@hotmail.com>
2021-07-19 09:22:44 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,10 +12,10 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Lienol/openwrt-package
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git' >>feeds.conf.default
-#echo 'src-git helloworld helloworld https://github.com/fw876/helloworld.git' >>feeds.conf.default
-#echo 'src-git poweroffdevice https://github.com/sirpdboy/luci-app-poweroffdevice.git' >>feeds.conf.default
-#echo 'src-git theme https://github.com/sirpdboy/luci-theme-opentopd.git' >>feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git helloworld helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git poweroffdevice https://github.com/sirpdboy/luci-app-poweroffdevice' >>feeds.conf.default
+echo 'src-git theme https://github.com/sirpdboy/luci-theme-opentopd' >>feeds.conf.default
 
 # AdGuard home
 #sed -i '$a src-git diy https://github.com/rufengsuixing/luci-app-adguardhome.git' feeds.conf.default
